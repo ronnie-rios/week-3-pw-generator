cfd31c028151d667ddf2fc33b647bb4ffe83572e
Ronnie Rios <ronniesmail14@gmail.com>
2021-04-10 13:40:19 -0500

    added if statemnts to add userinput to pw

--- script.js
+++ script.js
@@ -13,17 +13,39 @@ funtion generatePassword() {
   var passwordChar='';
 
   //user prompts
+  //pw length prompt
   var lengthPW = parseInt(prompt("Pick between 8 and 128 characters for your password."))
   if (lengthPW < 8 || lengthPW > 128) {
       alert("Password must be between 8 and 128 characters.")
       return '';
   }
-
+  //check if user wants upper, lower, numbers, or symbols in pw
   var checkLowerCase = confirm("Would you like to include lowercase?");
   var checkUppercase = confirm("Would you like to include uppercase?");
   var checkNumbers = confirm("Would you like to include numbers?");
   var checkSymbols = confirm("Would you like to include symbols?");
   
+  //if statement to check if user has selected at least 1 value for pw
+  if (!(checkUppercase||checkLowerCase||checkNumbers||checkSymbols)) {
+    alert("Your password must contain at least one uppercase, lowercase, number, or symbol characters");
+   return;
+  }
+  //if statements to add userinput to random pw
+  if (checkLowerCase) {
+    passwordChar += lowercaseChar;
+  }
+
+if (checkUppercase) {
+    passwordChar +=uppercaseChar;
+  }
+
+if (checkNumbers) {
+    passwordChar +=numericalChar;
+  }
+
+if (checkSymbols) {
+    passwordChar += specialChar;
+  }
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
